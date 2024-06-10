3098353837897dc33607aebff0ff8938e8adfc00
slacturyx <slacturyx@credil.org>
2024-06-10 13:30:03 -0400

    Update.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,86 +7,59 @@ import {
   PermissionsAndroid,
   Platform
 } from 'react-native'
-import { Central } from './Central'
-import { Peripheral } from './Peripheral'
-import { isBleEnabled } from '@animo-id/react-native-ble-didcomm'
-
-export const Spacer = () => <View style={{ height: 20, width: 20 }} />
-
-const requestPermissions = async () => {
-  await PermissionsAndroid.requestMultiple([
-    'android.permission.ACCESS_FINE_LOCATION',
-    'android.permission.BLUETOOTH_CONNECT',
-    'android.permission.BLUETOOTH_SCAN',
-    'android.permission.BLUETOOTH_ADVERTISE',
-    'android.permission.ACCESS_COARSE_LOCATION'
-  ])
-}
+// import { isBleEnabled } from '@animo-id/react-native-ble-didcomm'
+import { useEffect, useState } from 'react'
+import { Peripheral } from '@animo-id/react-native-ble-didcomm'
+import { Agent } from '@credo-ts/core'
+import { agentDependencies } from '@credo-ts/react-native'
 
 export default function App() {
-  const [isCentral, setIsCentral] = React.useState<boolean>(false)
-  const [isPeripheral, setIsPeripheral] = React.useState<boolean>(false)
+  const [peripheral, setPeripheral] = useState<Peripheral | undefined>(
+    undefined
+  )
+  const [agent, setAgent] = useState<Agent | undefined>(undefined)
 
-  const asCentral = () => setIsCentral(true)
+  const requestPermissions = async () => {
+    await PermissionsAndroid.requestMultiple([
+      'android.permission.ACCESS_FINE_LOCATION',
+      'android.permission.BLUETOOTH_CONNECT',
+      'android.permission.BLUETOOTH_SCAN',
+      'android.permission.BLUETOOTH_ADVERTISE',
+      'android.permission.ACCESS_COARSE_LOCATION'
+    ])
+  }
 
-  const asPeripheral = () => setIsPeripheral(true)
+  const startPeripheral = async () => {
+    const p = new Peripheral()
+
+    await p.start()
+    setPeripheral(p)
+  }
+
+  const initAgent = async () => {
+    const a = new Agent({
+      config: {
+        label: 'PC',
+        autoUpdateStorageOnStartup: true
+      },
+      modules: {},
+      dependencies: agentDependencies
+    })
+
+    await a.initialize()
+    setAgent(a)
+  }
+
+  useEffect(() => {
+    requestPermissions()
+    startPeripheral()
+    initAgent()
+  }, [])
 
   return (
-    <View style={styles.container}>
-      <Text>
-        Bluetooth demo screen. role:{' '}
-        {isCentral ? 'central' : isPeripheral ? 'peripheral' : 'none'}
-      </Text>
-      <Spacer />
-      <Button
-        title="is ble enabled"
-        onPress={async () => console.log(await isBleEnabled())}
-      />
-      {Platform.OS === 'android' && (
-        <>
-          <Button
-            title="requestPermissions"
-            onPress={async () => {
-              await requestPermissions()
-            }}
-          />
-        </>
-      )}
-      <Spacer />
-      {(isCentral || isPeripheral) && (
-        <>
-          <Button
-            title="Back"
-            onPress={() => {
-              setIsCentral(false)
-              setIsPeripheral(false)
-            }}
-          />
-          <Spacer />
-        </>
-      )}
-      {!isCentral && !isPeripheral && (
-        <>
-          <Button title="Central" onPress={asCentral} />
-          <Spacer />
-          <Button title="Peripheral" onPress={asPeripheral} />
-        </>
-      )}
-      {isCentral && <Central />}
-      {isPeripheral && <Peripheral />}
+    <View>
+      <Text>{peripheral && 'Peripheral is start'}</Text>
+      <Text>{agent?.isInitialized && 'Agent is initialized'}</Text>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20
-  }
-})
