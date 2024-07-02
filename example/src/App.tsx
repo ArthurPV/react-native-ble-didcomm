2aa9e8960a6c9e39f2929c736b3de94fc4a4aff2
slacturyx <slacturyx@credil.org>
2024-07-02 12:11:23 -0400

    Update example.

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { useEffect } from 'react'
 import {
   StyleSheet,
   View,
@@ -7,9 +8,10 @@ import {
   PermissionsAndroid,
   Platform
 } from 'react-native'
-import { Central } from './Central'
-import { Peripheral } from './Peripheral'
-import { isBleEnabled } from '@animo-id/react-native-ble-didcomm'
+import { isBleEnabled } from '../../src/bleStatus'
+import { Central, Peripheral, PeripheralProvider, CentralProvider } from '../../src/index'
+import NfcManager, {NfcTech, NfcEvents, TagEvent} from 'react-native-nfc-manager';
+import Child from './Child'
 
 export const Spacer = () => <View style={{ height: 20, width: 20 }} />
 
@@ -19,74 +21,16 @@ const requestPermissions = async () => {
     'android.permission.BLUETOOTH_CONNECT',
     'android.permission.BLUETOOTH_SCAN',
     'android.permission.BLUETOOTH_ADVERTISE',
-    'android.permission.ACCESS_COARSE_LOCATION'
+    'android.permission.ACCESS_COARSE_LOCATION',
   ])
 }
 
 export default function App() {
-  const [isCentral, setIsCentral] = React.useState<boolean>(false)
-  const [isPeripheral, setIsPeripheral] = React.useState<boolean>(false)
-
-  const asCentral = () => setIsCentral(true)
-
-  const asPeripheral = () => setIsPeripheral(true)
-
   return (
-    <View style={styles.container}>
-      <Text>
-        Bluetooth demo screen. role:{' '}
-        {isCentral ? 'central' : isPeripheral ? 'peripheral' : 'none'}
-      </Text>
-      <Spacer />
-      <Button
-        title="is ble enabled"
-        onPress={async () => console.log(await isBleEnabled())}
-      />
-      {Platform.OS === 'android' && (
-        <>
-          <Button
-            title="requestPermissions"
-            onPress={async () => {
-              await requestPermissions()
-            }}
-          />
-        </>
-      )}
-      <Spacer />
-      {(isCentral || isPeripheral) && (
-        <>
-          <Button
-            title="Back"
-            onPress={() => {
-              setIsCentral(false)
-              setIsPeripheral(false)
-            }}
-          />
-          <Spacer />
-        </>
-      )}
-      {!isCentral && !isPeripheral && (
-        <>
-          <Button title="Central" onPress={asCentral} />
-          <Spacer />
-          <Button title="Peripheral" onPress={asPeripheral} />
-        </>
-      )}
-      {isCentral && <Central />}
-      {isPeripheral && <Peripheral />}
-    </View>
+	  <PeripheralProvider  peripheral={new Peripheral()}>
+		  <CentralProvider central={new Central()}>
+			  <Child />
+		  </CentralProvider>
+	  </PeripheralProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20
-  }
-})
---
+++ example/src/Child.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react'
+import { useEffect, useState } from 'react'
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  PermissionsAndroid,
+  Platform
+} from 'react-native'
+import { useCentral, usePeripheral } from '../../src/index'
+import NfcManager, {NfcTech, NfcEvents, TagEvent, Ndef } from 'react-native-nfc-manager'
+
+export default function Child() {
+  const { central } = useCentral()
+  const { peripheral } = usePeripheral()
+
+  const [hasNfc, setHasNfc] = useState<boolean>(false)
+  const [isCentral, setIsCentral] = React.useState<boolean>(false)
+  const [isPeripheral, setIsPeripheral] = React.useState<boolean>(false)
+
+  const asCentral = () => setIsCentral(true)
+  const asPeripheral = () => setIsPeripheral(true)
+
+  useEffect(() => {
+	  const start = async () => {
+		  if (isCentral) {
+			  await central.start()
+		  } else if (isPeripheral) {
+			  await peripheral.start()
+		  }
+	  }
+
+	  start()
+  }, [isCentral, isPeripheral])
+
+  useEffect(() => {
+	const requestPermissions = async () => {
+	  await PermissionsAndroid.requestMultiple([
+		'android.permission.ACCESS_FINE_LOCATION',
+		'android.permission.BLUETOOTH_CONNECT',
+		'android.permission.BLUETOOTH_SCAN',
+		'android.permission.BLUETOOTH_ADVERTISE',
+		'android.permission.ACCESS_COARSE_LOCATION',
+	  ])
+	}
+
+	const checkIfNfcIsSupported = async () => {
+		setHasNfc(await NfcManager.isSupported())
+	}
+
+	const startNfc = async () => {
+		await NfcManager.start()
+
+		NfcManager.setEventListener(NfcEvents.DiscoverTag, async (tag: TagEvent) => {
+			console.log('Tag Discovered', tag);
+
+			await NfcManager.requestTechnology(NfcTech.Ndef)
+
+			if (isPeripheral) {
+				const bytes = Ndef.encodeMessage([Ndef.textRecord(await peripheral.getPeripheralAddress())])
+
+				if (bytes) {
+					await NfcManager.ndefHandler.writeNdefMessage(bytes)
+				}
+			} else if (isCentral) {
+				const receivedTag = await NfcManager.getTag()
+
+				if (receivedTag) {
+					const decodedMessages = receivedTag.ndefMessage.map(record => Ndef.text.decodePayload(record.payload as unknown as Uint8Array))
+
+					if (decodedMessages.length > 0) {
+						const peripheralId = decodedMessages[0]
+
+						console.log(`Received message: ${peripheralId}`)
+
+						await central.fastConnect(peripheralId)
+
+						console.log('Connected to peripheral.')
+					}
+				}
+			}
+
+			await NfcManager.cancelTechnologyRequest()
+			await NfcManager.unregisterTagEvent()
+		});
+	}
+
+	const stopNfc = async () => {
+		NfcManager.setEventListener(NfcEvents.DiscoverTag, null)
+
+		if (await NfcManager.isEnabled()) {
+			await NfcManager.unregisterTagEvent()
+		}
+	}
+
+	checkIfNfcIsSupported()
+
+	if (hasNfc) {
+		requestPermissions()
+		startNfc()
+	}
+
+	return () => {
+		if (hasNfc) {
+			stopNfc()
+		}
+	}
+  }, [isCentral, isPeripheral])
+
+  if (hasNfc) {
+	  return (
+		<View style={styles.container}> 
+		</View>
+	  )
+  }
+
+  return (
+	  <Text>NFC is not supported by this device.</Text>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20
+  }
+}
